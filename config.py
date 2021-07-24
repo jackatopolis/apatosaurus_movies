729f524259aeba530a9dc79fbbd1852cc3e8069c
Jack Cohen <80280642+jackatopolis@users.noreply.github.com>
2021-07-24 12:06:29 -0400

    Uploaded all movie data in all_movies dictionary

---
+++ config.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jul 24 11:19:09 2021
+
+@author: jackcohen
+"""
+
+api_key = 'ed6510db'
--- movie_list.py
+++ movie_list.py
@@ -1,5 +1,6 @@
 import requests as req
 from bs4 import BeautifulSoup
+from config import api_key
 
 # Scrape top 50 movies for each year between 1980-2020
 attr1 = {"class":"a-text-left mojo-field-type-release mojo-cell-wide"}
@@ -15,3 +16,6 @@ for j in range(1980,2021):
         movie = hello[i].text
         movies.append(movie)
     all_movies[j]=movies
+    
+    
+url = f"http://www.omdbapi.com/?apikey={api_key}&t="
